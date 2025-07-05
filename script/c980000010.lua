de0bc9c25ea3bf7779b24ddaa12d0fc946ae7525
Este98 <este98@hotmail.fr>
2025-07-05 17:01:49 +0200

    Added Suprem's King

---
+++ script/c980000010.lua
@@ -0,0 +1,19 @@
+--Supreme King's Castle
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--Dark Fusion Ignore
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(id)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetTargetRange(1,0)
+	c:RegisterEffect(e2)
+end
+s.listed_names={CARD_DARK_FUSION}
