8b355416ce5de6d7c1d35846ac13276b9848e76e
Este98 <este98@hotmail.fr>
2025-08-05 00:38:48 +0200

    Added Amazoness cards

---
+++ script/classic/c980000049.lua
@@ -0,0 +1,43 @@
+--Amazoness Princess
+local s,id=GetID()
+function s.initial_effect(c)
+	--This card's name becomes "Amazoness Queen" while on the field or in the GY
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_CHANGE_CODE)
+	e1:SetRange(LOCATION_MZONE|LOCATION_GRAVE)
+	e1:SetValue(15951532)
+	c:RegisterEffect(e1)
+	--Add 1 "Amazoness" Spell/Trap from your Deck to your hand
+	local e2a=Effect.CreateEffect(c)
+	e2a:SetDescription(aux.Stringid(id,0))
+	e2a:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2a:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2a:SetProperty(EFFECT_FLAG_DELAY)
+	e2a:SetCode(EVENT_SUMMON_SUCCESS)
+	e2a:SetCountLimit(1,id)
+	e2a:SetTarget(s.thtg)
+	e2a:SetOperation(s.thop)
+	c:RegisterEffect(e2a)
+	local e2b=e2a:Clone()
+	e2b:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2b)
+end
+s.listed_names={15951532,id} --"Amazoness Queen"
+s.listed_series={SET_AMAZONESS}
+function s.thfilter(c)
+	return c:IsSetCard(SET_AMAZONESS) and c:IsSpellTrap() and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
