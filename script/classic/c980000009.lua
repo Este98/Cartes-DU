1a6e1fe79f23765f3784d02ecd3abce398a8f51c
Este98 <este98@hotmail.fr>
2025-08-10 18:04:14 +0200

    Update c980000009.lua

--- script/classic/c980000009.lua
+++ script/classic/c980000009.lua
@@ -18,7 +18,7 @@ function s.initial_effect(c)
 	e3:SetCode(EFFECT_ADD_CODE)
 	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e3:SetRange(LOCATION_ONFIELD|LOCATION_GRAVE)
-	e3:SetValue(65622692)
+	e3:SetValue(64500000)
 	c:RegisterEffect(e3)
     --Union procedure
 	aux.AddUnionProcedure(c,aux.FilterBoolFunction(Card.IsRace,RACE_MACHINE))
