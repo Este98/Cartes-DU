dea9bc315ba4f5644b15af2c1c45ac69003b2d0e
Este98 <este98@hotmail.fr>
2025-10-14 19:58:39 +0200

    Added Dream Mirror Phantams

---
+++ script/classic/c980000149.lua
@@ -0,0 +1,28 @@
+--Dream Mirror Phantasms
+--Scripted by Eerie Code
+local s,id=GetID()
+function s.initial_effect(c)
+	--When activated, add 1 "Dream Mirror" monster from deck
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_series={SET_DREAM_MIRROR}
+s.listed_names={CARD_DREAM_MIRROR_JOY,CARD_DREAM_MIRROR_TERROR}
+function s.filter(c)
+	return c:IsSetCard(SET_DREAM_MIRROR) and c:IsMonster() and c:IsAbleToHand()
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_DECK,0,nil)
+	if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=g:Select(tp,1,1,nil)
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg)
+	end
+end
