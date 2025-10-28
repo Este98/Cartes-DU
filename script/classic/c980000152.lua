dae8884b7bde716c18ab1ced0e889672024d83cf
Este98 <este98@hotmail.fr>
2025-10-28 18:05:49 +0100

    Added new Masked Hero Cards
    Masked HERO Dian
    Masked HERO Blast
    Contrast HERO Chaos

---
+++ script/classic/c980000152.lua
@@ -0,0 +1,13 @@
+--Masked HERO Dian
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Special summon condition
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(aux.FALSE)
+	c:RegisterEffect(e1)
+end
+s.listed_series={SET_HERO}
