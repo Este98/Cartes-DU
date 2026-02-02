a01d1ee6ca41cd679de57d610a75d0c5116c9f65
Este98 <este98@hotmail.fr>
2026-02-02 07:20:52 +0100

    Added new cards
    The Bistro Butcher
    Hungry Fries

--- script/rush/c983000090.lua
+++ script/rush/c983000090.lua
@@ -1,5 +1,14 @@
---Hungry Burger
+--Hungry Burger (Rush)
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
+    --Change name to "Hungry Burger"
+    local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CHANGE_CODE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_ALL)
+	e1:SetValue(30243636)
+	c:RegisterEffect(e1)
 end
+s.listed_names={80811661} --"Hamburger Recipe"
---
+++ script/rush/c983000100.lua
@@ -0,0 +1,54 @@
+--Hungry Fries
+local s,id=GetID()
+function s.initial_effect(c)
+	--Add to the hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+    --If you control no monsters, or all monsters you control are "The Bistro Butcher" and/or "Hungry" monsters, you can Normal Summon this card without Tributing.
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e2:SetCode(EFFECT_SUMMON_PROC)
+	e2:SetCondition(s.sumcon)
+	c:RegisterEffect(e2)
+end
+s.listed_series={0xf008} --"Hungry" archetype
+s.listed_names={71107816} --"The Bistrot Butcher"
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsSummonPhaseMain() and c:IsStatus(STATUS_SUMMON_TURN+STATUS_SPSUMMON_TURN)
+end
+function s.thfilter(c)
+	return (c:IsCode(71107816) or c:IsSetCard(0xf008) and c:IsMonster()) and c:IsAbleToHand()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Effect
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.thfilter),tp,LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function s.sumconfilter(c)
+	return c:IsFacedown() or not (c:IsSetCard(0xf008) or c:IsCode(71107816))
+end
+function s.sumcon(e,c,minc,zone)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return minc==0 and c:IsLevelAbove(5) and Duel.GetLocationCount(tp,LOCATION_MZONE,tp,LOCATION_REASON_TOFIELD,zone)>0
+		and (Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0 or not Duel.IsExistingMatchingCard(s.sumconfilter,tp,LOCATION_MZONE,0,1,nil))
+end
